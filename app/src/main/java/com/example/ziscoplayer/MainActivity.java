2ac9014fff3617666c1fa40011f7d5dbcc20bcd6
SURANJAN <sdclouddevelopment@gmail.com>
2022-01-10 19:36:38 +0530

    git commit -m "Second commit - Searching and listing(ArrayList implementation) songs after storage permission"

--- app/src/main/java/com/example/ziscoplayer/MainActivity.java
+++ app/src/main/java/com/example/ziscoplayer/MainActivity.java
@@ -2,13 +2,77 @@ package com.example.ziscoplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.ArrayList;
+
+//To use runtime permission we use dexter library
 public class MainActivity extends AppCompatActivity {
-
+    ListView listview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listview = findViewById(R.id.listview);
+        Dexter.withContext(MainActivity.this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+//                        Toast.makeText(MainActivity.this, "Runtime Permission Given", Toast.LENGTH_SHORT).show();
+                        ArrayList<File> mySongs = fetchSongs(Environment.getExternalStorageDirectory());
+                        String [] items = new String[mySongs.size()];
+                        for(int i=0;i<mySongs.size();i++){
+                            items[i] = mySongs.get(i).getName().replace(".mp3",""); //To truncate .mp3 part only keep the name
+                        }
+                        //using listview
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, items);
+                        listview.setAdapter(adapter);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                })
+                .check();
     }
+
+    public ArrayList<File> fetchSongs(File file){
+        ArrayList arrayList = new ArrayList();  //we will have an arrayList to store all mp3 here
+        File[] songs = file.listFiles(); //List of directories in file
+        if (songs != null){ //if the file containing directories is not empty
+            for(File myFile : songs){
+                if(!myFile.isHidden() && myFile.isDirectory()){  //IF it is not a hidden directory, then recursively run the method fetchSongs to get into the directories
+                    arrayList.addAll(fetchSongs(myFile));
+                }
+                else{
+                    if(myFile.getName().endsWith(".mp3") && !myFile.getName().startsWith(".")){
+                        arrayList.add(myFile); //If current file is a mp3 file, add it to the arrayList
+                    }
+                }
+            }
+        }
+        return arrayList;
+    }
+
+
 }
